7ffd4753bb961365897dacbf4bdfb8f3697f7a43
rcmonitor <rcmonitor@mail.ru>
2017-05-25 23:54:11 +0300

    Add test for production code

--- executor_test.go
+++ executor_test.go
@@ -6,7 +6,6 @@ import (
 	"github.com/rcmonitor/executor"
 	"github.com/rcmonitor/executor/testdata"
 	"github.com/romana/rlog"
-	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	"testing"
 )
@@ -20,8 +19,11 @@ type TTestSuiteExecute struct {
 	Executor *executor.TFramedExecutor
 }
 
-func (suite *TTestSuiteExecute) TestFramedExecutorTestdata() {
+func (suite *TTestSuiteExecute) SetupTest() {
 	suite.Executor = &executor.TFramedExecutor{}
+}
+
+func (suite *TTestSuiteExecute) ExcludedTestFramedExecutorTestdata() {
 
 	suite.mSetTestWorkers()
 
@@ -59,106 +61,27 @@ func (suite *TTestSuiteExecute) mSetTestWorkers() {
 
 }
 
-/*
 
-func ExcludedTestTFramedJob_MRun_Primal(t *testing.T) {
-	sWorker := &TPrimalWorker{10}
-	sFactory := provider.TTitleFactory{&provider.TSDBMService{}}
-	cService, err := sFactory.MGet("test_service")
-	require.Nil(t, err)
-	require.NotNil(t, cService)
-	psdbmService, ok := cService.(*provider.TSDBMService)
-	require.True(t, ok)
-	require.NotNil(t, psdbmService)
+func (suite *TTestSuiteExecute) TestTFramedExecutorProduction() {
 
-	sJob := executor.TFramedExecutor{
-		Service: psdbmService,
-		Worker: sWorker,
-	}
-	sJob.MRun()
-
-	time.Sleep(10 * time.Second)
-
-	bRunning := psdbmService.MIsRunning()
-	require.True(t, bRunning)
-
-	time.Sleep(10 * time.Second)
-	err = psdbmService.MStop()
-	require.Nil(t, err)
-
-	time.Sleep(10 * time.Second)
-	bRunning = psdbmService.MIsRunning()
-	require.False(t, bRunning)
-}
-
-*/
-
-/*
-
-func Excluded_TestTFramedJob_MRun_ParseDownload(t *testing.T) {
-	psWorker := &parser.TWorkerParse{&parser.TDownload{}, parser.TParser{}}
-
-	psdbmService, err := provider.FGetServiceByTitleCached("parser_download")
-
-	require.Nil(t, err)
-	require.NotNil(t, psdbmService)
-
-	sJob := executor.TFramedExecutor{
-		Service: psdbmService,
-		Worker:  psWorker,
-		//LogConf: "conf/service_log.conf",
-		//LogDir: "log/",
-	}
-	sJob.MRun()
-
-	//fCheckRunStop(t, psdbmService)
-}
-
-func Excluded_TestTFramedJob_MRun_ParseProxy(t *testing.T) {
-	psWorker := &parser.TWorkerParse{&parser.TProxy{}, parser.TParser{}}
-
-	psdbmService, err := provider.FGetServiceByTitleCached("parser_proxy")
-
-	require.Nil(t, err)
-	require.NotNil(t, psdbmService)
-
-	sJob := executor.TFramedExecutor{
-		Service: psdbmService,
-		Worker: psWorker,
-	}
-
-	sJob.MRun()
-
-	//fCheckRunStop(t, psdbmService)
-}
-
-*/
-
-func ExcludedTestTFramedExecutor_MRun(t *testing.T) {
-	sExecutor := executor.TFramedExecutor{}
-
-	fSetWorkers(t, &sExecutor)
+	suite.mSetProductionWorkers()
 
 	intWorkersExpected := 2
+	suite.Require().Equal(intWorkersExpected, len(suite.Executor.SW))
 
-	require.Equal(t, intWorkersExpected, len(sExecutor.SW))
-
-	//t.Logf("Length of ServiceWorker slice is: %d", len(sExecutor.SW))
-	//t.Logf("type of first is: %T", sExecutor.SW[0])
-	//t.Fatalf("type of second is: %T", sExecutor.SW[1])
-
-	sExecutor.MRun()
+	err := suite.Executor.MRun()
+	suite.Require().Nil(err)
 
 }
 
-func fSetWorkers(t *testing.T, psExecutor *executor.TFramedExecutor) {
+func (suite *TTestSuiteExecute) mSetProductionWorkers() {
 	pswParseProxy := &parser.TWorkerParse{&parser.TProxy{}, parser.TParser{}}
 
 	psdbmsParserProxy, err := provider.FGetServiceByTitleCached("parser_proxy")
-	require.Nil(t, err)
-	require.NotNil(t, psdbmsParserProxy)
+	suite.Require().Nil(err)
+	suite.Require().NotNil(psdbmsParserProxy)
 
-	psExecutor.SW = append(psExecutor.SW, &executor.TServiceWorker{
+	suite.Executor.SW = append(suite.Executor.SW, &executor.TServiceWorker{
 		Service: psdbmsParserProxy,
 		Worker:  pswParseProxy,
 	})
@@ -166,37 +89,15 @@ func fSetWorkers(t *testing.T, psExecutor *executor.TFramedExecutor) {
 	pswParseDownload := &parser.TWorkerParse{&parser.TDownload{}, parser.TParser{}}
 
 	psdbmsParserDownload, err := provider.FGetServiceByTitleCached("parser_download")
-	require.Nil(t, err)
-	require.NotNil(t, psdbmsParserDownload)
+	suite.Require().Nil(err)
+	suite.Require().NotNil(psdbmsParserDownload)
 
-	psExecutor.SW = append(psExecutor.SW, &executor.TServiceWorker{
+	suite.Executor.SW = append(suite.Executor.SW, &executor.TServiceWorker{
 		Service: psdbmsParserDownload,
 		Worker:  pswParseDownload,
 	})
 }
 
-/*
-
-func fCheckRunStop(t *testing.T, psdbmService *provider.TSDBMService) {
-	time.Sleep(1 * time.Minute)
-
-	boolRunning := psdbmService.MIsRunning()
-	require.True(t, boolRunning)
-
-	err := psdbmService.MStop()
-	require.Nil(t, err)
-
-	pstPeriod, err := psdbmService.MGetPeriod()
-	require.Nil(t, err)
-	require.NotNil(t, pstPeriod)
-
-	time.Sleep(*pstPeriod)
-
-	boolRunning = psdbmService.MIsRunning()
-	require.False(t, boolRunning)
-}
-
-*/
 
 func TestExecutorSuite(t *testing.T) {
 	suite.Run(t, new(TTestSuiteExecute))
